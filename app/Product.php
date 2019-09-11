87d21d0db69f48acb42e98d8aa4362127a5722a6
lypsisrudiansyah <rudiansyahether@gmail.com>
2019-09-11 18:26:07 +0700

    Adding Global Scope to few Models, Adding SoftDeletes and Make Trait ApiResponse
    [ADD] Global Scope To Buyer and Seller
    
    [ADD] SoftDeletes to Migrations And Model
    
    [ADD] Traits ApiResponse

--- app/Product.php
+++ app/Product.php
@@ -6,9 +6,13 @@ use App\Category;
 use App\Seller;
 use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+	use SoftDeletes;
+	protected $dates =['deleted_at'];
+	
 	const AVAILABLE_PRODUCT = 'available';
 	const UNAVAILABLE_PRODUCT = 'unavailable';
 
--- app/User.php
+++ app/User.php
@@ -3,13 +3,17 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
+    use SoftDeletes;
     use Notifiable;
+    
+    protected $dates =['deleted_at'];
 
     const VERIFIED_USER = '1';
     const UNVERIFIED_USER = '0';
@@ -52,6 +56,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+
+    public function getNameAttribute($name)
+    {
+        return ucwords($name);
+    }
+
+    public function setEmailAttribute($email)
+    {
+        $this->attributes['email'] = strtolower($email);
+    }
 
     public function isVerified()
     {
